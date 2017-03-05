3cdf0adafae39e337d604a8c3c8b8ebcefea11b0
flan <flan@flande.re>
2017-03-05 01:19:23 +0100

    Remove irrelevant test
    To add a test for the new check, #35 would be needed to be merged
    first.

--- tests/test_sync_app.py
+++ tests/test_sync_app.py
@@ -28,34 +28,6 @@ class SyncCollectionHandlerTest(CollectionTestBase):
         CollectionTestBase.tearDown(self)
         self.syncCollectionHandler = None
 
-    def test_meta(self):
-        version_info = (None,
-                        ','.join(('ankidesktop', '2.0.12', 'lin::')),
-                        ','.join(('ankidesktop', '2.0.32', 'lin::')))
-
-        meta = self.syncCollectionHandler.meta(version_info[0])
-        self.assertEqual(meta[0], self.collection.mod)
-        self.assertEqual(meta[1], self.collection.scm)
-        self.assertEqual(meta[2], self.collection._usn)
-        self.assertTrue((type(meta[3]) == int) and meta[3] > 0)
-        self.assertEqual(meta[4], self.collection.media.lastUsn())
-
-        meta = self.syncCollectionHandler.meta(version_info[1])
-        self.assertEqual(meta[0], self.collection.mod)
-        self.assertEqual(meta[1], self.collection.scm)
-        self.assertEqual(meta[2], self.collection._usn)
-        self.assertTrue((type(meta[3]) == int) and meta[3] > 0)
-        self.assertEqual(meta[4], self.collection.media.lastUsn())
-
-        meta = self.syncCollectionHandler.meta(version_info[2])
-        self.assertEqual(meta['scm'], self.collection.scm)
-        self.assertTrue((type(meta['ts']) == int) and meta['ts'] > 0)
-        self.assertEqual(meta['mod'], self.collection.mod)
-        self.assertEqual(meta['usn'], self.collection._usn)
-        self.assertEqual(meta['musn'], self.collection.media.lastUsn())
-        self.assertEqual(meta['msg'], '')
-        self.assertEqual(meta['cont'], True)
-
 
 class SimpleUserManagerTest(unittest.TestCase):
     _good_test_un = 'username'
