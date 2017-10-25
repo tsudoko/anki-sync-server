5f648fa0dc0815e02241462fd8bd819060f70d93
flan <flan@flande.re>
2017-10-25 13:27:06 +0200

    Change Python version classifier in setup.py to 3

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Education',
         'Topic :: Education :: Computer Aided Instruction (CAI)',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
